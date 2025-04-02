ea76cb6596a7ec7e4474d53746b6ee7c90856c20
Carlos <carlosalonsocamina@gmail.com>
2025-04-02 16:26:05 +0300

    Add Random functionality

--- Section 04 - Swift Programming Basics - Collections, Constants & Variables/Dicee/Dicee App/ViewController.swift
+++ Section 04 - Swift Programming Basics - Collections, Constants & Variables/Dicee/Dicee App/ViewController.swift
@@ -13,20 +13,12 @@ class ViewController: UIViewController {
     @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        diceImageView1.image = #imageLiteral(resourceName: "DiceSix")
-        diceImageView2.image = #imageLiteral(resourceName: "DiceTwo")
-        
-        
-    }
-
     @IBAction func rollButtonPressed(_ sender: UIButton) {
-        print ("Button got tapped. ")
-
-        diceImageView1.image = #imageLiteral(resourceName: "DiceFour")
-        diceImageView2.image = #imageLiteral(resourceName: "DiceFour")
+        
+        let diceArray = [ #imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix") ]
+        
+        diceImageView1.image = diceArray [Int.random(in: 0...5)]
+        diceImageView2.image = diceArray [Int.random(in: 0...5)]
     }
     
 }
