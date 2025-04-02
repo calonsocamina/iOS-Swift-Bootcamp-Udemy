71ebf1730d5e51b8c0df1305fccfa0232b448d13
Carlos <carlosalonsocamina@gmail.com>
2025-04-02 13:40:02 +0300

    Button interaction on UI elements

--- Section 04 - Swift Programming Basics - Collections, Constants & Variables/Dicee/Dicee App/ViewController.swift
+++ Section 04 - Swift Programming Basics - Collections, Constants & Variables/Dicee/Dicee App/ViewController.swift
@@ -11,14 +11,23 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         diceImageView1.image = #imageLiteral(resourceName: "DiceSix")
-        diceImageView1.alpha = 0.5
+        diceImageView2.image = #imageLiteral(resourceName: "DiceTwo")
+        
+        
     }
 
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        print ("Button got tapped. ")
 
+        diceImageView1.image = #imageLiteral(resourceName: "DiceFour")
+        diceImageView2.image = #imageLiteral(resourceName: "DiceFour")
+    }
+    
 }
 
