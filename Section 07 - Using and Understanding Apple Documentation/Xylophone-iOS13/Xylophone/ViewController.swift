9b32b3509f6951fb658a63f5168c766c02104d72
Carlos <carlosalonsocamina@gmail.com>
2025-04-03 16:38:10 +0300

    Completion Xylophone project. Added sound to each button and change of opacity when pressing them

--- Section 07 - Using and Understanding Apple Documentation/Xylophone-iOS13/Xylophone/ViewController.swift
+++ Section 07 - Using and Understanding Apple Documentation/Xylophone-iOS13/Xylophone/ViewController.swift
@@ -7,15 +7,46 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-
+    @IBAction func keyPressed(_ sender: UIButton) {
+        
+        sender.alpha = 0.5
+        
+        //Dependiendo del nombre del boton, aplica su sonido correspondiente
+        playSound(soundName: sender.currentTitle!)
+        
+        //After 0.2 seconds return to original opacity
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+            sender.alpha = 1.0
+        }
+    }
     
+    //Parametro soundName
+    func playSound(soundName: String) {
+        guard let url = Bundle.main.url(forResource: soundName, withExtension: "wav") else { return }
 
+        do {
+            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+            try AVAudioSession.sharedInstance().setActive(true)
+
+            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
+
+            guard let player = player else { return }
+
+            player.play()
+
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }
 }
 
