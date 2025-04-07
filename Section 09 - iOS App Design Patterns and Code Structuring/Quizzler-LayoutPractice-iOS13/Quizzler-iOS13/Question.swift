8511216602ba3a87c42a67d9428b5003b860ebd9
Carlos <carlosalonsocamina@gmail.com>
2025-04-07 12:14:44 +0300

    Added Timer functionality to buttons for change color depending on user answer

---
+++ Section 09 - iOS App Design Patterns and Code Structuring/Quizzler-LayoutPractice-iOS13/Quizzler-iOS13/Question.swift
@@ -0,0 +1,19 @@
+//
+//  Question.swift
+//  Quizzler-iOS13
+//
+//  Created by Carlos Alonso Camiña on 7/4/25.
+//  Copyright © 2025 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct Question {
+    let text: String
+    let answer: String
+    
+    init(q: String, a: String) {
+        text = q
+        answer = a
+    }
+}
