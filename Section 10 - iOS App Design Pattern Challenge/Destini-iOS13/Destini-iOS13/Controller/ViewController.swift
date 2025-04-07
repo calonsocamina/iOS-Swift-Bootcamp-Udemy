9e1ab8baa6fd4f5d7291b1cd1d8a745b2217bb65
Carlos <carlosalonsocamina@gmail.com>
2025-04-07 16:59:40 +0300

    Completed Destini project from Section 10

--- Section 10 - iOS App Design Pattern Challenge/Destini-iOS13/Destini-iOS13/Controller/ViewController.swift
+++ Section 10 - iOS App Design Pattern Challenge/Destini-iOS13/Destini-iOS13/Controller/ViewController.swift
@@ -14,22 +14,26 @@ class ViewController: UIViewController {
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
-    let example = Story(title: "You see a fork in the road.",
-    choice1: "Take a left",
-    choice2: "Take a right")
+    var storyBrain = StoryBrain()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        storyLabel.text = story0
-        choice1Button.setTitle(choice1, for: .normal)
-        choice2Button.setTitle(choice2, for: .normal)
+        updateUI()
         
-
     }
 
     @IBAction func choiceMade(_ sender: UIButton) {
-        let user
+        
+        storyBrain.nextStory(userChoice: sender.currentTitle!)
+        
+        updateUI()
+    }
+    
+    func updateUI() {
+        storyLabel.text = storyBrain.getStoryTitle()
+        choice1Button.setTitle(storyBrain.getChoice1(), for: .normal)
+        choice2Button.setTitle(storyBrain.getChoice2(), for: .normal)
     }
     
 }
