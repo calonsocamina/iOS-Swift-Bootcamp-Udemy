e304755d150c6a839d61d848e0d74ea9372a231e
Carlos <carlosalonsocamina@gmail.com>
2025-04-07 16:52:54 +0300

    Implemented Model part (Story and StoryBrain)

--- Section 10 - iOS App Design Pattern Challenge/Destini-iOS13/Destini-iOS13/Controller/ViewController.swift
+++ Section 10 - iOS App Design Pattern Challenge/Destini-iOS13/Destini-iOS13/Controller/ViewController.swift
@@ -14,11 +14,23 @@ class ViewController: UIViewController {
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
+    let example = Story(title: "You see a fork in the road.",
+    choice1: "Take a left",
+    choice2: "Take a right")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        storyLabel.text = story0
+        choice1Button.setTitle(choice1, for: .normal)
+        choice2Button.setTitle(choice2, for: .normal)
+        
 
     }
 
-
+    @IBAction func choiceMade(_ sender: UIButton) {
+        let user
+    }
+    
 }
 
