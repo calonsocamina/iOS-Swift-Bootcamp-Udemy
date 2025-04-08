b71a72d8e6fb26df496de3cc0e5b4b57638eff47
Carlos <carlosalonsocamina@gmail.com>
2025-04-08 15:01:38 +0300

    Completion BMI Calculator project

--- Section 11 - Advanced Swift Programming: Classes, Inheritance & Advanced Optionals/BMI-Calculator-iOS13/BMI Calculator/Controllers/CalculateViewController.swift
+++ Section 11 - Advanced Swift Programming: Classes, Inheritance & Advanced Optionals/BMI-Calculator-iOS13/BMI Calculator/Controllers/CalculateViewController.swift
@@ -10,17 +10,15 @@ import UIKit
 
 class CalculateViewController: UIViewController {
     
-    var bmiValue = "0.0"
-    
+    var calculatorBrain = CalculatorBrain()
+
     @IBOutlet weak var heightLabel: UILabel!
     @IBOutlet weak var weightLabel: UILabel!
-    
     @IBOutlet weak var heightSlider: UISlider!
     @IBOutlet weak var weightSlider: UISlider!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
     @IBAction func heightSliderChanged(_ sender: UISlider) {
@@ -30,24 +28,24 @@ class CalculateViewController: UIViewController {
     
     @IBAction func weightSliderChanged(_ sender: UISlider) {
         let weight = String(format: "%.0f", sender.value)
-        weightLabel.text = " \(weight)kg"
+        weightLabel.text = "\(weight)Kg"
     }
     
     @IBAction func calculatePressed(_ sender: UIButton) {
         let height = heightSlider.value
         let weight = weightSlider.value
-        
-        let bmi = weight / pow(height, 2)
-        bmiValue = String(format: "%.1f", bmi)
-        
+
+        calculatorBrain.calculateBMI(height: height, weight: weight)
         self.performSegue(withIdentifier: "goToResult", sender: self)
+        
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "goToResult" {
-            let destinationVC = segue.destination as! ResultsViewController
-            destinationVC.bmiValue = bmiValue
+            let destinationVC = segue.destination as! ResultViewController
+            destinationVC.bmiValue = calculatorBrain.getBMIValue()
+            destinationVC.advice = calculatorBrain.getAdvice()
+            destinationVC.color = calculatorBrain.getColor()
         }
     }
 }
-
---
+++ Section 11 - Advanced Swift Programming: Classes, Inheritance & Advanced Optionals/BMI-Calculator-iOS13/BMI Calculator/Models/BMI.swift
@@ -0,0 +1,15 @@
+//
+//  File.swift
+//  BMI Calculator
+//
+//  Created by Carlos Alonso Camiña on 8/4/25.
+//  Copyright © 2025 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+struct BMI {
+    let value: Float
+    let advice: String
+    let color: UIColor
+}
