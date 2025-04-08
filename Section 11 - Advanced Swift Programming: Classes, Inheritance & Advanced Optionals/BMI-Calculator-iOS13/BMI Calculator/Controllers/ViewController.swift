3db1a8a0437065d6824c1bf303c74dbe037eb689
Carlos <carlosalonsocamina@gmail.com>
2025-04-08 11:47:33 +0300

    Working with Class, inheritance and multiscreen on Section 11

--- Section 11 - Advanced Swift Programming: Classes, Inheritance & Advanced Optionals/BMI-Calculator-iOS13/BMI Calculator/Controllers/ViewController.swift
+++ Section 11 - Advanced Swift Programming: Classes, Inheritance & Advanced Optionals/BMI-Calculator-iOS13/BMI Calculator/Controllers/ViewController.swift
@@ -12,6 +12,9 @@ class ViewController: UIViewController {
     @IBOutlet weak var heightLabel: UILabel!
     @IBOutlet weak var weightLabel: UILabel!
     
+    @IBOutlet weak var heightSlider: UISlider!
+    @IBOutlet weak var weightSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -21,9 +24,22 @@ class ViewController: UIViewController {
         let height = String(format: "%.2f", sender.value)
         heightLabel.text = "\(height)m"
     }
+    
     @IBAction func weightSliderChanged(_ sender: UISlider) {
         let weight = String(format: "%.0f", sender.value)
         weightLabel.text = " \(weight)kg"
+    }
+    
+    @IBAction func calculatePressed(_ sender: UIButton) {
+        let height = heightSlider.value
+        let weight = weightSlider.value
+        
+        let bmi = weight / pow(height, 2)
+        
+        let secondVC = SecondViewController()
+        secondVC.bmiValue = String(format: "%.1g", bmi)
+        self.present(secondVC, animated: true, completion: nil)
+        
     }
 }
 
