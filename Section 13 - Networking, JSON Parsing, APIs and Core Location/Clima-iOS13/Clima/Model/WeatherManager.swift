11958b6e02b30e899c459e6e6895cd4cb50bf927
Carlos <carlosalonsocamina@gmail.com>
2025-04-10 10:49:58 +0300

    Closure, JSON decoding and Computed Properties

--- Section 13 - Networking, JSON Parsing, APIs and Core Location/Clima-iOS13/Clima/Model/WeatherManager.swift
+++ Section 13 - Networking, JSON Parsing, APIs and Core Location/Clima-iOS13/Clima/Model/WeatherManager.swift
@@ -21,27 +21,39 @@ struct WeatherManager {
     func performRequest(urlString: String) {
         //1. Create a URL
         if let url = URL(string: urlString) {
+            
             //2. Create a URLSession
             let session = URLSession(configuration: .default)
             
             //3. Give the session a task
-            let task = session.dataTask(with: url, completionHandler: handle(data:reponse:error:))
+            let task = session.dataTask(with: url) { data, response, error in
+                if error != nil {
+                    print(error!)
+                    return
+                }
+                if let safeData = data {
+                    parseJSON(weatherData: safeData)
+                }
+            }
             
             //4. Start the task
             task.resume()
         }
     }
-    
-    //Function destinated to cover the completionHandler when we give a task to the session on performRequest()
-    func handle(data: Data?, reponse: URLResponse?, error: Error?) {
-        if error != nil {
-            print(error!)
-            return
-        }
-        
-        if let safeData = data {
-            let dataString = String(data: safeData, encoding: .utf8)
-            print(dataString)
+    func parseJSON(weatherData: Data) {
+        let decoder = JSONDecoder()
+        do{
+           let decodedData = try decoder.decode(WeatherData.self, from: weatherData)
+            let id = decodedData.weather[0].id
+            let temp = decodedData.main.temp
+            let name = decodedData.name
+            
+            let weather = WeatherModel(conditionId: id, cityName: name, temperature: temp)
+            
+            print(weather.temperatureString)
+            
+        } catch {
+            print(error)
         }
     }
 }
