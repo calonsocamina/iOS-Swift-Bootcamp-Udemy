c4b820d7dc718e858055676a28dc4cadd70ed590
Carlos <carlosalonsocamina@gmail.com>
2025-04-14 16:30:42 +0300

    CocoaPod, CLTypingLabel, Apple Swift Packages, Firebase implementation, login and logout process, creation of Constants file

--- Section 15 - Firebase Cloud Firestore, TableViews and Cocoapod Dependencies/Flash-Chat-iOS13/Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ Section 15 - Firebase Cloud Firestore, TableViews and Cocoapod Dependencies/Flash-Chat-iOS13/Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -7,23 +7,28 @@
 //
 
 import UIKit
+import CLTypingLabel
 
 class WelcomeViewController: UIViewController {
 
-    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var titleLabel: CLTypingLabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        titleLabel.text = ""
-        var charIndex = 0.0
-        let titleText = "⚡️FlashChat"
-        for letter in titleText {
-            Timer.scheduledTimer(withTimeInterval: 0.1 * charIndex, repeats: false) { (timer) in
-                self.titleLabel.text?.append(letter)
-            }
-            charIndex += 1
-        }
+        //Animation of text in one line of code with CLTypingLabel library
+        titleLabel.text = K.appName
+        
+        //Same effect coding manually
+//        titleLabel.text = ""
+//        var charIndex = 0.0
+//        let titleText = "⚡️FlashChat"
+//        for letter in titleText {
+//            Timer.scheduledTimer(withTimeInterval: 0.1 * charIndex, repeats: false) { (timer) in
+//                self.titleLabel.text?.append(letter)
+//            }
+//            charIndex += 1
+//        }
     }
     
 
