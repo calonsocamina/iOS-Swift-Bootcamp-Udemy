9e86cc1927cc72a87ca4cf700d8171efd4565802
Carlos <carlosalonsocamina@gmail.com>
2025-04-16 12:21:01 +0300

    Completed Dicee-SwiftUI project from Section 17

--- Section 17 - SwiftUI and Declarative Programming/Dice-SwiftUI/Dice-SwiftUI/ContentView.swift
+++ Section 17 - SwiftUI and Declarative Programming/Dice-SwiftUI/Dice-SwiftUI/ContentView.swift
@@ -8,6 +8,10 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var leftDiceNumber: Int = 1
+    @State var rightDiceNumber: Int = 1
+    
     var body: some View {
         ZStack{
             Image("background")
@@ -17,13 +21,14 @@ struct ContentView: View {
                 Image("diceeLogo")
                 Spacer() //Space between logo and dices
                 HStack {
-                    DiceView(n: 1)
-                    DiceView(n: 2)
+                    DiceView(n: leftDiceNumber)
+                    DiceView(n: rightDiceNumber)
                 }
                 .padding(.horizontal)
                 Spacer() //Space between dices and button
                 Button(action: {
-                    
+                    self.leftDiceNumber = Int.random(in: 1...6)
+                    self.rightDiceNumber = Int.random(in: 1...6)
                 }) {
                     Text("Roll")
                         .font(.system(size: 50))
